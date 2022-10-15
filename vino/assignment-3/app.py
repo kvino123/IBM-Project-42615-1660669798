b9388d5572a2085860e6d4dd7f9ae49a31926226
kvino123 <113660246+kvino123@users.noreply.github.com>
2022-10-15 21:13:43 +0530

    Add files via upload

---
+++ vino/assignment-3/app.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/signin")
+def signin():
+    return render_template("signin.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
